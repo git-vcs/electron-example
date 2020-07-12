5397c753ce083823fdef0b686c165de3a3053d2e
Mikael <58994869+git-vcs@users.noreply.github.com>
2020-07-12 12:07:42 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         Learn React
-        </a>
+        
           <a>This app is made to test build pipline for an electron app using react</a>
       </header>
     </div>
